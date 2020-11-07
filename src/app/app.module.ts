35df3de9cfda1ab5609af6548aa59c9db98f9f65
vitalii-8D <gotbubu0@gmail.com>
2020-11-08 01:40:47 +0200

    - html in proccess

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ChatBaseService} from './services/chat-base.service';
+import { ChatComponent } from './components/chat/chat.component';
+import { SideSectionComponent } from './components/side-section/side-section.component';
+import { ChatZoneComponent } from './components/chat-zone/chat-zone.component';
+import { UserComponent } from './components/user/user.component';
+import { UserIntroComponent } from './components/user-intro/user-intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    SideSectionComponent,
+    ChatZoneComponent,
+    UserComponent,
+    UserIntroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [ChatBaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCC1StLtZsfCC5ANhgi5-2vfYw7roL9c78',
+    authDomain: 'almost-live-chat.firebaseapp.com',
+    databaseURL: 'https://almost-live-chat.firebaseio.com',
+    projectId: 'almost-live-chat',
+    storageBucket: 'almost-live-chat.appspot.com',
+    messagingSenderId: '168388370205',
+    appId: '1:168388370205:web:324e6f96b7dfbb030a0bf4'
+  }
 };
 
 /*
