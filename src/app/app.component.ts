40994378df77aa8ddd51cc2ba469aa5dbe4680e1
vitalii-8D <gotbubu0@gmail.com>
2020-11-16 22:55:34 +0200

    - Fixing bugs - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {DataBaseService} from './services/data-base.service';
 import {AngularFirestore} from '@angular/fire/firestore';
 import firebase from 'firebase';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'almost-live-chat';
 
-  constructor() {
+export class AppComponent {
+
+  constructor(private router: Router) {
   }
+
+  // Reaction for back btn
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event): void {
+    this.router.navigate(['/chat']);
+    console.log('Back button pressed');
+  }
+
 }
